536d18db4e269acb7d21496bcb67d45b3ff2b47e
Rishabh Shrivastava <116031457+rishabhshri162@users.noreply.github.com>
2026-01-12 18:25:17 +0530

    done

--- src/main/java/in/co/rays/project_3/model/ModelFactory.java
+++ src/main/java/in/co/rays/project_3/model/ModelFactory.java
@@ -27,21 +27,21 @@ public final class ModelFactory {
 			mFactory = new ModelFactory();
 		}
 		return mFactory;
-	}
+	 }
 
-	public HostelRoomModelInt getHostelRoomModel() {
-		HostelRoomModelInt hostelRoomModel = (HostelRoomModelInt) modelCache.get("hostelRoomModel");
-		if (hostelRoomModel == null) {
-			if ("Hibernate".equals(DATABASE)) {
-				hostelRoomModel = new HostelRoomModelHibImp();
-			}
-			if ("JDBC".equals(DATABASE)) {
-				hostelRoomModel = new HostelRoomModelJDBCImpl();
-			}
-			modelCache.put("hostelRoomModel", hostelRoomModel);
-		}
-		return hostelRoomModel;
-	}
+	// public HostelRoomModelInt getHostelRoomModel() {
+	// 	HostelRoomModelInt hostelRoomModel = (HostelRoomModelInt) modelCache.get("hostelRoomModel");
+	// 	if (hostelRoomModel == null) {
+	// 		if ("Hibernate".equals(DATABASE)) {
+	// 			hostelRoomModel = new HostelRoomModelHibImp();
+	// 		}
+	// 		if ("JDBC".equals(DATABASE)) {
+	// 			hostelRoomModel = new HostelRoomModelJDBCImpl();
+	// 		}
+	// 		modelCache.put("hostelRoomModel", hostelRoomModel);
+	// 	}
+	// 	return hostelRoomModel;
+	// }
 
 	public ProductModelInt getProductModel() {
 		ProductModelInt productModel = (ProductModelInt) modelCache.get("productModel");

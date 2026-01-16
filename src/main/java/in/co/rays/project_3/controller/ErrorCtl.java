e675f11d775763cf0de4d18bc3aac7f1e5114221
Rishabh Shrivastava <116031457+rishabhshri162@users.noreply.github.com>
2026-01-16 17:51:17 +0530

    Update ErrorCtl.java

--- src/main/java/in/co/rays/project_3/controller/ErrorCtl.java
+++ src/main/java/in/co/rays/project_3/controller/ErrorCtl.java
@@ -6,41 +6,48 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import in.co.rays.project_3.util.ServletUtility;
 
 /**
  * Error functionality controller.perform error page operation
- * 
- * @author Rishabh Shrivastava
+ * @author Chaitanya Bhatt
  *
  */
-@WebServlet(name = "ErrorCtl", urlPatterns = { "/ErrorCtl" })
-public class ErrorCtl extends BaseCtl {
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException {
-
-				HttpSession session = request.getSession(true);
-
-			session = request.getSession();
-			session.invalidate();
-
-		ServletUtility.setErrorMessage("Database server down", request);
-		ServletUtility.forward(getView(), request, response);
-
-	}
+@WebServlet(name="ErrorCtl", urlPatterns={"/ErrorCtl"})
+public class ErrorCtl extends BaseCtl{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	
+	 /**
+     * Concept of Display logic
+     *
+     */
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException{
+		HttpSession session = request.getSession(true);
+		session = request.getSession();
+		session.invalidate();
+		ServletUtility.setErrorMessage("Database server down!!!", request);
 		ServletUtility.forward(getView(), request, response);
+		
 	}
-
+	 /**
+     * Concept of submit logic
+     *
+     */
+ protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException{
+	
+  ServletUtility.forward(getView(), request, response);
+ 
+   
+ }
 	@Override
 	protected String getView() {
 		return ORSView.LOGIN_VIEW;
 	}
-
 
 }
